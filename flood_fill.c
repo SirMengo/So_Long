6bbd156f44c1d85ce1bec4507d438f8ba15ff3a4
Miguel Simoes <msimoes0103@gmail.com>
2025-08-18 16:40:18 +0100

    Maps now accepting wholes on it (It shouldnt)

--- flood_fill.c
+++ flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: msimoes <msimoes@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 11:57:04 by msimoes           #+#    #+#             */
-/*   Updated: 2025/08/18 14:48:06 by msimoes          ###   ########.fr       */
+/*   Updated: 2025/08/18 15:35:10 by msimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	check_cpy(char **map_cpy)
 {
-	printf("%s", map_cpy[0]);
-	printf("%s", map_cpy[1]);
-	printf("%s", map_cpy[2]);
-	printf("%s", map_cpy[3]);
-	printf("%s", map_cpy[4]);
+	printf("\n%s\n", map_cpy[0]);
+	printf("%s\n", map_cpy[1]);
+	printf("%s\n", map_cpy[2]);
+	printf("%s\n", map_cpy[3]);
+	printf("%s\n", map_cpy[4]);
 
 }
 
@@ -45,16 +45,16 @@ void	locate_player(t_map *map)
 	map_cpy = map->map;
 	x = 0;
 	y = 0;
-	while (map_cpy[y][x])
+	while (map_cpy[y] != NULL)
 	{
-		y++;
-		while (map_cpy[y][x])
+		x = 0;
+		while (map_cpy[y][x] != '\0')
 		{
 			if (map_cpy[y][x] == 'P')
 				flood_fill(map, map_cpy, x, y);
 			x++;
 		}
-		x = 0;
+		y++;
 	}
 	check_cpy(map_cpy);
 }
